3c88b2e41ee33ec68a8b59d904b5e2036ad9810b
Brooks <brooks@pterodactyl>
2012-04-14 14:12:12 -0500

    Fixed prompt to execute __git_ps1 everytime, not just upon first sourcing

--- .bash.d/prompt.bash
+++ .bash.d/prompt.bash
@@ -25,7 +25,7 @@ color_none="\[\e[0m\]"
 
 # set the prompt
 #
-export PS1="$color_jobs\j$color_none $color_user\u$color_none $color_host\h$color_none $color_git$(__git_ps1)$color_none $color_dir\w$color_none "
+export PS1="$color_jobs\j$color_none $color_user\u$color_none $color_host\h$color_none $color_git\$(__git_ps1 '%s')$color_none $color_dir\w$color_none "
 
 # Set the title to user@host:dir
 #
